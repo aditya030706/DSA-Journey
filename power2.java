58378d95ad1da74735eec35bfaf45fbebce26095
aditya030706 <aditya030706@gmail.com>
2026-08-14 18:38:36 +0530

    Add power2 solution

---
+++ power2.java
@@ -0,0 +1,28 @@
+public class power2 {
+    
+    public static boolean isPowerOfTwo(int n) {
+
+        if (n <= 0) {
+            return false;
+        }
+
+        while (n > 1) {
+            if (n % 2 != 0) {
+                return false;
+            }
+            n = n / 2;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        int n = 16;
+
+        boolean result = isPowerOfTwo(n);
+
+        System.out.println(result);
+    }
+    
+}
