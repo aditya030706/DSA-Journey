2b5eb235fed0cfa5fabdc9512815805da07e9b01
aditya030706 <aditya030706@gmail.com>
2026-08-14 19:09:09 +0530

    Add fibonacci solution

---
+++ fibonnaci.java
@@ -0,0 +1,21 @@
+public class fibonnaci {
+    
+    public static int fibonacci(int n) {
+
+        if (n <= 1) {
+            return n;
+        }
+
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+
+        int n = 10;
+
+        int result = fibonacci(n);
+
+        System.out.println(result);
+    }
+    
+}
