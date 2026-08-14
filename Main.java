86cfd07004a88c46b8b151180bd71f219e965835
aditya030706 <aditya030706@gmail.com>
2026-08-14 16:27:19 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,32 @@
+public class Main {
+
+    public static boolean isPalindrome(int x) {
+
+        if (x < 0) {
+            return false;
+        }
+
+        int original = x;
+        int reverse = 0;
+
+        while (x != 0) {
+
+            int digit = x % 10;
+
+            reverse = reverse * 10 + digit;
+
+            x = x / 10;
+        }
+
+        return original == reverse;
+    }
+
+    public static void main(String[] args) {
+
+        int x = 121;
+
+        boolean result = isPalindrome(x);
+
+        System.out.println(result);
+    }
+}
